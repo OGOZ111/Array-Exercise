e9f5c34a10db984d825f26a46f0734cb49dfead2
Luke B <142310217+OGOZ111@users.noreply.github.com>
2023-12-07 11:31:46 +0200

    Add files via upload

---
+++ Class.js
@@ -0,0 +1,62 @@
+/*class Pizza {
+    constructor(name, toppings, price) {
+      this.name = name;
+      this.price = price
+      this.toppings = toppings;
+    }
+
+      getnum(price) 
+      {return 
+    
+    }
+
+    }*/
+
+  class Pizza {
+    name;
+    toppings = []
+    basePrice = 0;
+    size = 'S'
+  }  
+
+  let deliveryType = 0
+  let totalprice = 0
+
+
+  class PizzaOrder {
+    customerName = '';
+    deliveryType = 'EAT_IN'
+    pizzas = [];
+  }  
+
+
+  getPrice (){
+    if (deliveryType == 'Deliver') {200 += totalprice}
+    }
+  }
+
+
+
+
+
+
+    class Pizza {
+        name;
+        toppings = [];
+        basePrice = 0;
+        size = 'S';
+
+        constructor(name, toppings, basePrice){
+
+        }
+
+        getPrice() {
+
+
+        }
+    }
+
+
+  
+    const americano = new Pizza();
+    americano.name = 'americano';
